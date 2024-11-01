fd05a9ce1b00138ed35c0a0c074378f30f12df0a
Safi89-E <tafakor.safi@gmail.com>
2024-11-01 11:23:31 +0330

    Add files via upload

---
+++ NewSaveAs.py
@@ -0,0 +1,70 @@
+from PyQt6.QtWidgets import (QApplication,QLabel,
+                             QWidget, QPushButton,
+                             QTextEdit,QVBoxLayout)
+from PyQt6.QtGui import QColor
+import subprocess
+import os
+app = QApplication([])
+window = QWidget()
+layout = QVBoxLayout()
+
+Save = QPushButton("Save as")
+
+FileName = QTextEdit()
+#FileName.setGeometry(50,50,300,10)
+FileName.label = QLabel("File name:")
+FileName.setTextColor(QColor("blue"))
+FileName.setPlaceholderText("Write your file name here...")
+
+FilePath =QTextEdit()
+FilePath.label = QLabel("File path:")
+
+FilePath.setTextColor(QColor("red"))
+FilePath.setPlaceholderText("Write your file path here...")
+Guide = QTextEdit()
+Guide.setStyleSheet("QTextEdit { background-color: gray; color: white; }")
+layout.addWidget(Save)
+layout.addWidget(Guide)
+layout.addWidget(FileName.label)
+layout.addWidget(FileName)
+layout.addWidget(FilePath.label)
+layout.addWidget(FilePath)
+
+Guide.setReadOnly(True)
+window.setWindowTitle("Save as")
+
+
+window.setLayout(layout)
+#def read():
+    #with open("empty.py", 'r') as file:  
+            #content = file.read()
+    
+def Save_as():
+    counter = 1
+    #content = read()
+    File_name = FileName.toPlainText()
+    File_path = FilePath.toPlainText()
+    if os.path.exists(f"newfile{counter}.txt"):
+            counter += 1
+            with open(f"newfile{counter}.txt", "w") as f:
+             f.write("Helo!!")
+            
+    else:
+            with open(f"newfile{counter}.txt", "w") as f:
+             f.write("Helo!!")
+    '''if os.path.isfile(File_name):
+            Guide.setText(f"There is {File_name}")
+        else:
+            os.path.dirname(File_path)
+            with open(f"{File_name}.txt", "w") as f:
+                f.write("defad")
+'''
+
+        
+
+
+Save.clicked.connect(Save_as)
+
+
+window.show()
+app.exec()
